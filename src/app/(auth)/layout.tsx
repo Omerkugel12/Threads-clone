7fd34321eb5786a7df46aeb1b42d1e935fcf0ca1
Omer Kugel <omerkugel12@gmail.com>
2025-03-09 18:21:52 +0200

    initialization

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: "Threads",
+  description: "Threads",
+};
+
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import React from "react";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    // <ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className} bg-dark-1`}>{children}</body>
+    </html>
+    // </ClerkProvider>
+  );
+}
+
+export default RootLayout;
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div className="bg-white">page</div>;
+}
+
+export default page;
