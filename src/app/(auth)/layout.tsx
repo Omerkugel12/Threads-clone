a9b79902345da13b977f47b498554d5be545737d
Omer Kugel <omerkugel12@gmail.com>
2025-03-10 10:20:25 +0200

    clerk - signIn

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -12,11 +12,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    // <ClerkProvider>
-    <html lang="en">
-      <body className={`${inter.className} bg-dark-1`}>{children}</body>
-    </html>
-    // </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
 
