cf742a66e2a8cca40cea09782e4c47ce4c284d43
Omer Kugel <omerkugel12@gmail.com>
2025-03-19 19:32:52 +0200

    fix-userId

--- src/lib/models/thread.model.ts
+++ src/lib/models/thread.model.ts
@@ -5,28 +5,14 @@ const threadSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  author: {
-    type: String,
-    unique: true,
-    required: true,
-  },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   community: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Community",
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  parentId: {
-    type: String,
-  },
-  children: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Thread",
-    },
-  ],
+  createdAt: { type: Date, default: Date.now },
+  parentId: { type: String },
+  children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thread" }],
 });
 
 const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
