595a4f71e382cead651df7c04c09c2ececa8c60c
Omer Kugel <omerkugel12@gmail.com>
2025-03-19 20:28:38 +0200

    fetch-threads

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,42 @@
+import { fetchPosts } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 5);
+
+  const user = await currentUser();
+  if (!user) return null;
+
   return (
     <main>
-      <h1 className="">Threads</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p>No Threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => {
+              return (
+                <>
+                  {/* <ThreadCard
+                    key={post._id}
+                    id={post._id}
+                    currentUserId={user?.id}
+                    parentId={post.parentId}
+                    content={post.text}
+                    author={post.author}
+                    community={post.community}
+                    createdAt={post.createdAt}
+                    comments={post.children}
+                  /> */}
+                </>
+              );
+            })}
+          </>
+        )}
+      </section>
     </main>
   );
 }
