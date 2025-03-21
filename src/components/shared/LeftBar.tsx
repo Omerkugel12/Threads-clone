493f739c214d8eb370223f701665e58dc4ddb98e
Omer Kugel <omerkugel12@gmail.com>
2025-03-21 19:30:00 +0200

    profile-header

--- src/components/shared/LeftBar.tsx
+++ src/components/shared/LeftBar.tsx
@@ -12,7 +12,6 @@ function LeftSideBar() {
   const pathname = usePathname();
 
   const { userId } = useAuth();
-  console.log(userId);
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
