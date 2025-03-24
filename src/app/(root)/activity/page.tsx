a6c790130de7a692962095e006e1664dc267d7c2
Omer Kugel <omerkugel12@gmail.com>
2025-03-24 14:29:04 +0200

    get-activity-backend

--- src/app/(root)/activity/page.tsx
+++ src/app/(root)/activity/page.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function Page() {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const activity = await getActivity(userInfo._id);
+  console.log(activity);
+
   return <div>Activity</div>;
 }
 
