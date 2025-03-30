ef97d31b6818bf756bc4940c330b6b2cc935532b
Omer Kugel <omerkugel12@gmail.com>
2025-03-30 13:03:44 +0300

    check-4

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,20 +5,20 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 async function Home() {
-  // const result = await fetchPosts(1, 5);
+  const result = await fetchPosts(1, 5);
 
-  // const user = await currentUser();
-  // if (!user) return null;
+  const user = await currentUser();
+  if (!user) return null;
 
-  // const userInfo = await fetchUser(user.id);
-  // if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
   return (
     <main>
       <h1 className="head-text text-left">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
-        {/* {result.posts.length === 0 ? (
+        {result.posts.length === 0 ? (
           <p>No Threads found</p>
         ) : (
           <>
@@ -39,7 +39,7 @@ async function Home() {
               );
             })}
           </>
-        )} */}
+        )}
       </section>
     </main>
   );
