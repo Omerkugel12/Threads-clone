101b27996db8f91d468a85528c2fc58da055f714
Omer Kugel <omerkugel12@gmail.com>
2025-04-01 13:22:44 +0300

    times-for-each-thread

--- src/components/cards/ThreadCard.tsx
+++ src/components/cards/ThreadCard.tsx
@@ -107,25 +107,24 @@ function ThreadCard({
           </div>
         </div>
       </div>
-      {isComment && community && (
-        <Link
-          href={`/communities/${community.id}`}
-          className="mt-5 flex items-center"
-        >
-          <p className="text-subtle-medium text-gray-1">
-            {formatDateString(createdAt)}
-            {community && ` - ${community.name} Community`}
-          </p>
-
+      <Link
+        href={`/communities/${community?.id}`}
+        className="mt-5 flex items-center"
+      >
+        <p className="text-subtle-medium text-gray-1">
+          {formatDateString(createdAt)}
+          {community && ` - ${community.name} Community`}
+        </p>
+        {isComment && community && (
           <Image
             src={community.image}
-            alt={community.name}
+            alt={community?.name}
             width={14}
             height={14}
             className="ml-1 rounded-full object-cover"
           />
-        </Link>
-      )}
+        )}
+      </Link>
     </article>
   );
 }
